1c223caafadeb6451d59cbb662de19a70cb11a8f
mahdee <khmahdee@gmail.com>
2020-09-04 14:19:39 +0430

    add httpprofiles 13.6.99

---
+++ lib/http/httpProfiles.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:stubbbb/Models/profileModels.dart';
+
+class ProfileHttp {
+  static Future<Map> getData() async {
+    List<User> profiles = [];
+    var response = await http.get('http://192.168.1.5/Stub/userGetData.php');
+
+    var responseBody = json.decode(response.body);
+    responseBody.forEach((item) {
+      profiles.add(User.fromjson(item));
+    });
+    return {
+      'profiles' : profiles
+      };
+  }
+}
